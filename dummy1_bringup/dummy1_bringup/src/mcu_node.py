6118760cf4f03cc9e3015cc210f51b242b2aae71
Gone030 <gjkjw_030@naver.com>
2022-03-15 05:21:18 -0700

    change_plan

--- dummy1_bringup/dummy1_bringup/src/mcu_node.py
+++ dummy1_bringup/dummy1_bringup/src/mcu_node.py
@@ -21,27 +21,34 @@ class ros22mcu(Node):
         self.get_logger().info("Connecting port")
         _port_name = self.get_parameter_or('/port/name', Parameter('/port/name', Parameter.Type.STRING, '/dev/ttyACM0')).get_parameter_value().string_value
         _port_baudrate = self.get_parameter_or('/port/baudrate', Parameter('/port/baudrate', Parameter.Type.INTEGER, 115200)).get_parameter_value().integer_value
-        qos_profile = QoSProfile(
-            depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
+        # qos_profile = QoSProfile(
+        #     depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
 
-        self.subvel = self.create_subscription(Twist, 'cmd_vel', self.decision ,qos_profile)
-        self.pubImu = self.create_publisher(Imu, 'imu', qos_profile)
-        self.pubOdo = self.create_publisher(Odometry, 'odom', qos_profile)
-
+        # # self.subvel = self.create_subscription(Twist, 'cmd_vel', self.decision ,qos_profile)
+        # self.pubImu = self.create_publisher(Imu, 'imu', qos_profile)
+        # self.pubOdo = self.create_publisher(Odometry, 'odom', qos_profile)
         self.ser = None
-        self.open_port(_port_name, _port_baudrate)
-        self.imu_angular_vel_x = self.read_data('Roll')
-        self.get_logger().info(self.imu_angular_vel_x)
-        self.imu_angular_vel_y = self.read_data('Pitch')
-        self.get_logger().info(self.imu_angular_vel_y)
-        self.imu_angular_vel_z = self.read_data('Yaw')
-        self.get_logger().info(self.imu_angular_vel_z)
-        self.imu_linear_acc_x = self.read_data('Ax')
-        self.get_logger().info(self.imu_linear_acc_x)
-        self.imu_linear_acc_y = self.read_data('Ay')
-        self.get_logger().info(self.imu_linear_acc_y)
-        self.imu_linear_acc_z = self.read_data('Az')
-        self.get_logger().info(self.imu_linear_acc_z)
+        self.ser = serial.Serial(_port_name, _port_baudrate)
+        self.timerprocess = self.create_timer(0.05, self.serialcomm)
+
+    def serialcomm(self):
+        # a = self.get_clock().now()
+        # self.imu_angular_vel_x = self.read_data('Roll')
+        # self.imu_angular_vel_y = self.read_data('Pitch')
+        # self.imu_angular_vel_z = self.read_data('Yaw')
+        # self.imu_linear_acc_x = self.read_data('Ax')
+        # self.imu_linear_acc_y = self.read_data('Ay')
+        # self.imu_linear_acc_z = self.read_data('Az')
+        # self.ser.
+        # self.get_logger().info("test1")
+        # comm = 'c'
+        # self.ser.write(comm.encode())
+        if self.ser.readable():
+            self.responsedata = self.ser.readline()
+            self.data = self.responsedata[1:len(self.responsedata)-1].decode()
+        self.get_logger().info(self.data)
+        # b = self.get_clock().now()
+        # self.get_logger().info('looptime : %s' %(b-a))
 
     #     timestamp_now = self.get_clock().now().to_msg()
     #     imu_msg = Imu()
@@ -127,52 +134,16 @@ class ros22mcu(Node):
 
     #     self.write_data(str(self.target_angular +'!'+self.target_linear))
 
-    def read_data(self, cmd):
-        self.clear_port()
-        _temp = 0
-        self.write_port(cmd)
-        if self.ser != None:
-            if self.ser.isOpen():
-                if _temp[0] == '@' and '\r\n' in _temp:
-                    _temp = self.ser.readline()
-                    _temp = _temp.decode('utf-8')[1:-2]
-        return _temp
-
-    def write_port(self, buf):
-        _reset = 0
-        if self.ser != None:
-            if self.ser.isOpen():
-                _reset = self.ser.write(buf)
-        return _reset
-
-    def open_port(self, port_name, baudrate):
-        _port_name = port_name
-        _baudrate = baudrate
-        self.close_port()
-        self.ser = serial.Serial(_port_name, _baudrate)
-
-    def close_port(self):
-        if self.ser != None:
-            if self.ser.isOpen():
-                self.ser.close()
-
-    def clear_port(self):
-        if self.ser != None:
-            if self.ser.isOpen():
-                self.ser.flush()
-
-    def write_data(self, argv_d):
-        self.clear_port()
-        argv_d = '&' + argv_d + '\r\n'
-        argv = argv_d.encode('utf-8')
-        self.write_port(argv)
-
 def main(args=None):
     rclpy.init(args=args)
     node_= ros22mcu()
-    rclpy.spin(node_)
+    try:
+        rclpy.spin(node_)
+    except KeyboardInterrupt:
+        node_.get_logger().info('bye')
+    finally:
+        node_.destroy_node()
+        rclpy.shutdown()
 
-    node_.destroy_node()
-    rclpy.shutdown()
 if __name__=='__main__':
     main()
--- script/src/main.cpp
+++ script/src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
-#include <String.h>
 
 int cmd_anguler_vel = 0;
 int cmd_linear_vel = 0;
-
+String inputstring = "";
+boolean commcomplete = false;
 void setup()
 {
   Serial.begin(115200);
@@ -11,35 +11,26 @@ void setup()
 
 void loop()
 {
-  String temp = "";
-  if (Serial.available())
-  {
-    temp = Serial.readStringUntil('\r');
-  }
-  int first = temp.indexOf("&");
-  int lenth = temp.length();
+  // if(commcomplete)
+  // {
+    // Serial.println("M" + inputstring);
+    // inputstring = "";
+    // Serial.println("Mdasdasd");
+    // commcomplete = false;
+  // }
+  // if(Serial.available())
+  // 
+  //   char input = (char)Serial.read();
+  //   if (input == 'c')
+  //   {
+      Serial.println("Megseafadf");
 
-  int check = temp.indexOf("!");
-  if (first != -1)
-  {
-    if (check > 0)
-    {
-      String angular_vel = temp.substring(first, check);
-      cmd_anguler_vel = angular_vel.toInt();
-      String linear_vel = temp.substring(check + 1, lenth);
-      cmd_linear_vel = linear_vel.toInt();
-    }
-    if (temp.indexOf("Roll") > -1)
-      Serial.println("@Hi");
-    else if (temp.indexOf("@Pitch") > -1)
-      Serial.println("@I'm");
-    else if (temp.indexOf("@Yaw") > -1)
-      Serial.println("@ji");
-    else if (temp.indexOf("@Ax") > -1)
-      Serial.println("@won");
-  }
-  else
-  {
-    temp = "";
-  }
+  // while(Serial.available())
+  // {
+  //   inputstring = Serial.readString();
+  //   Serial.println("M" + inputstring);
+  // }
 }
+
+
+
