32659c30f0e3745746a3d117f5046bf8aa7f9870
Gone030 <gjkjw_030@naver.com>
2022-03-28 02:44:34 +0900

    typo correction

--- test_due/lib/Motor/Motor.cpp
+++ test_due/lib/Motor/Motor.cpp
@@ -1,13 +1,9 @@
 #include "Motor.h"
-#include <DuePWM.h>
-#include <Servo.h>
+#include "Servo.h"
 
-#define PWM_DC 20000
-#define PWM_SV 1000
 #define MAX_SERVO_ANGLE 2.0944 //rad
 #define MIN_SERVO_ANGLE 1.0472 //rad
 
-DuePWM pwm(PWM_DC, PWM_SV);
 Servo steering_servo;
 control::control(int pwm_pin, int motor_pin_a, int motor_pin_b, int servo_pin )
 {
@@ -20,25 +16,14 @@ control::control(int pwm_pin, int motor_pin_a, int motor_pin_b, int servo_pin )
     pinMode(motor_pin_B_, OUTPUT);
     pinMode(pwm_pin_, OUTPUT); 
 
-    pwm.pinFreq1(pwm_pin_);
-    pwm.setFreq1(PWM_DC);
-    
-    pwm.pinDuty(pwm_pin_, 0);
     steering_servo.attach(servo_pin_);
 
 }
 
 void control::run(int pwm_duty)
 {   
-    bool dir;
-    if(pwm_duty >= 0)
-        dir = true;
-    else
-        dir = false;
-
     pwm_duty_ = pwm_duty;
-    pwm_duty_ = myMap(pwm_duty_, 0, 255);
-    if(dir)
+    if(pwm_duty_)
     {
         digitalWrite(motor_pin_A_, HIGH);
         digitalWrite(motor_pin_B_, LOW);
@@ -48,7 +33,7 @@ void control::run(int pwm_duty)
         digitalWrite(motor_pin_A_, LOW);
         digitalWrite(motor_pin_B_, HIGH);
     }
-    pwm.pinDuty(pwm_pin_, pwm_duty_);
+    analogWrite(pwm_pin_, pwm_duty_);
 }
 
 float control::steer(float steering_angle)
