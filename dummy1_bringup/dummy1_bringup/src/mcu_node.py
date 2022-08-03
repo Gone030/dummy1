0865281351a384e4fa6c3d278c3c99576e76cbdf
Gone030 <gjkjw_030@naver.com>
2022-08-04 02:59:08 +0900

    node fix

--- dummy1_bringup/dummy1_bringup/src/mcu_node.py
+++ dummy1_bringup/dummy1_bringup/src/mcu_node.py
@@ -8,10 +8,16 @@ from rclpy.parameter import Parameter
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Int32
 
 import math
 import numpy as np
 
+global prv_num
+global missing_num
+
+prv_num = 0
+missing_num = []
 
 class ros22mcu(Node):
 
@@ -23,32 +29,25 @@ class ros22mcu(Node):
         # self.subvel = self.create_subscription(Twist, 'cmd_vel', self.decision ,qos_profile)
         # self.subOdovel = self.create_subscription(Odometry, 'odom_velo',self.odometry, qos_profile)
         self.subImu = self.create_subscription(Imu, 'imu_due', self.imucontrol, qos_profile)
-        # self.pubImu = self.create_publisher(Imu, 'imu', qos_profile)
+        self.pubImu = self.create_publisher(Imu, 'imu/data', qos_profile)
         # self.pubOdo = self.create_publisher(Odometry, 'odom', qos_profile)
+        self.subtest = self.create_subscription(Int32, 'test_publisher', self.testcallback, qos_profile)
 
     def imucontrol(self, imu):
-        self.get_logger().info('msg receive')
-        # self.imu_angular_vel_x = imu.angular_velocity.x
-        # self.imu_angular_vel_y = imu.angular_velocity.y
-        # self.imu_angular_vel_z = imu.angular_velocity.z
-        # self.imu_linear_acceleration_x = imu.linear_acceleration.x
-        # self.imu_linear_acceleration_y = imu.linear_acceleration.y
-        # self.imu_linear_acceleration_z = imu.linear_acceleration.z
-        # timestamp_now = self.get_clock().now().to_msg()
-        # imu_msg = Imu()
-        # imu_msg.header.frame_id = 'imu_link'
-        # imu_msg.header.stamp = timestamp_now
-        # imu_msg.angular_velocity.x = float(self.imu_angular_vel_x)
-        # imu_msg.angular_velocity.y = float(self.imu_angular_vel_y)
-        # imu_msg.angular_velocity.z = float(self.imu_angular_vel_z)
-        # imu_msg.linear_acceleration.x = float(self.imu_linear_acc_x)
-        # imu_msg.linear_acceleration.y = float(self.imu_linear_acc_y)
-        # imu_msg.linear_acceleration.z = float(self.imu_linear_acc_z)
-        # self.get_logger().info('angular_v_x = {0}'.format(imu_msg.linear_acceleration.x))
-        # self.get_logger().info('angular_v_y = {0}'.format(imu_msg.linear_acceleration.y))
-        # self.get_logger().info('angular_v_z = {0}'.format(imu_msg.linear_acceleration.z))
-        # self.pubImu.publish(imu_msg)
+        timestamp_now = self.get_clock().now().to_msg()
+        imu_msg = imu
+        imu_msg.header.frame_id = 'imu_base'
+        imu_msg.header.stamp = timestamp_now
+        self.pubImu.publish(imu_msg)
+    def testcallback(self, msg):
+        global prv_num
+        global missing_num
 
+        if msg.data != prv_num + 1:
+            missing_num.extend(list(range(prv_num + 1, msg.data)))
+            print("              missed: {0}".format(missing_num))
+        prv_num = msg.data
+        print("              Receive: {0:>3}".format(msg.data))
 
     # def odometry(self, vel):
     #     linear_vel_x = vel.linear_x
@@ -58,7 +57,7 @@ class ros22mcu(Node):
     #     self.y_pose = 0
     #     self.theta = 0
     #     self.now_time = self.get_clock().now()
-    #     self.dt = (self.now_time - self.prev_time).nanosecond * 1e-9
+    #     self.dt = (s elf.now_time - self.prev_time).nanosecond * 1e-9
     #     self.prev_time = self.now_time
     #     self.dtheta = angular_vel_z * self.dt
     #     self.costh = math.cos(self.theta)
