5faaeea7d2049f04365bd6ca1b23d2ad6e0cb4e9
Gone030 <gjkjw_030@naver.com>
2022-08-04 02:58:53 +0900

    imu & comm test

--- script/src/main.cpp
+++ script/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+
 #include <micro_ros_arduino.h>
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
@@ -8,6 +9,7 @@
 
 // #include <geometry_msgs/msg/twist.h>
 #include <sensor_msgs/msg/imu.h>
+#include <std_msgs/msg/int32.h> //test
 // #include <nav_msgs/msg/odometry.h>
 
 #include "Calculates.h"
@@ -18,6 +20,7 @@
 // rcl_publisher_t odom_pub;
 rcl_publisher_t imu_pub;
 rcl_subscription_t imu_sub;
+rcl_publisher_t test_pub;
 
 rclc_executor_t executor;
 rclc_support_t support;
@@ -29,6 +32,7 @@ unsigned long long time_offset = 0;
 
 // geometry_msgs__msg__Twist twist_msg;
 sensor_msgs__msg__Imu imu_msg;
+std_msgs__msg__Int32 testmsg;
 // nav_msgs__msg__Odometry odom_msg;
 
 Imu imu;
@@ -187,7 +191,8 @@ void publishData()
   imu_msg.header.stamp.nanosec = time_stamp.tv_nsec;
 
   RCSOFTCHECK(rcl_publish(&imu_pub, &imu_msg, NULL));
-
+  RCSOFTCHECK(rcl_publish(&test_pub, &testmsg, NULL));
+  testmsg.data++;
 }
 
 void controlcallback(rcl_timer_t *timer, int64_t last_call_time)
@@ -220,6 +225,7 @@ void move()
   //temperary value
 }
 */
+
 bool createEntities()
 {
   allocator = rcl_get_default_allocator();
@@ -240,14 +246,21 @@ bool createEntities()
     ROSIDL_GET_MSG_TYPE_SUPPORT(nav_msgs, msg, Odometry),
     "odom_velo"));
   */
+
   RCCHECK(rclc_publisher_init_best_effort(
     &imu_pub,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Imu),
     "imu_due" //수정 요
   ));
+  RCCHECK(rclc_publisher_init_best_effort(
+    &test_pub,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "test_publisher"
+  ));
 
-  // For actuating the motor at 20 KHz (temp)
+  // For actuating the motor at 20 KHz (temp) 0.05
   const unsigned int timeout = 0.05;
   RCCHECK(rclc_timer_init_default(
     &control_timer,
@@ -255,7 +268,7 @@ bool createEntities()
     RCL_MS_TO_NS(timeout),
     controlcallback
   ));
-
+  executor = rclc_executor_get_zero_initialized_executor();
   RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
   /*
   RCCHECK(rclc_executor_add_subscription(
@@ -266,6 +279,8 @@ bool createEntities()
     ON_NEW_DATA
   ));
   */
+  RCCHECK(rclc_executor_add_timer(&executor, &control_timer));
+ testmsg.data = 0;
  syncTime();
  return true;
 }
@@ -276,6 +291,10 @@ bool destroyEntities()
   (void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
 
   rcl_publisher_fini(&imu_pub, &node);
+  rcl_publisher_fini(&test_pub,&node);
+  rclc_executor_fini(&executor);
+  rclc_support_fini(&support);
+  rcl_timer_fini(&control_timer);
   //나머지 채워놓을것
   return true;
 }
@@ -286,6 +305,7 @@ void setup()
   pinMode(pB, INPUT);
   attachInterrupt(pZ, encoderReset, FALLING);
   */
+
   bool imu_test = imu.init();
   if(!imu_test)
   {
@@ -336,88 +356,3 @@ void loop()
   }
 }
 /////////////////////////////////////////////////////////////////
-/*
-#include <micro_ros_arduino.h>
-#include <Arduino.h>
-#include <stdio.h>
-#include <rcl/rcl.h>
-#include <rcl/error_handling.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-
-#include <std_msgs/msg/int32.h>
-
-rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
-rclc_executor_t executor;
-rclc_support_t support;
-rcl_allocator_t allocator;
-rcl_node_t node;
-rcl_timer_t timer;
-
-#define LED_PIN 13
-
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
-
-
-void error_loop(){
-  while(1){
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-    delay(100);
-  }
-}
-
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-{
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.data++;
-  }
-}
-
-void setup() {
-  set_microros_transports();
-
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-
-  delay(2000);
-
-  allocator = rcl_get_default_allocator();
-  //create init_options
-  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
-  // create node
-  rcl_node_options_t node_ops = rcl_node_get_default_options();
-  node_ops.domain_id = 7;
-  RCCHECK(rclc_node_init_with_options(&node, "micro_ros_arduino_node", "", &support, &node_ops));
-
-  // create publisher
-  RCCHECK(rclc_publisher_init_default(
-    &publisher,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "arduino2ros"));
-
-  // create timer,
-  const unsigned int timer_timeout = 1000;
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
-
-  // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
-
-  msg.data = 0;
-}
-
-void loop() {
-  delay(100);
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
-}
-*/
--- test_due/src/main.cpp
+++ test_due/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+/*
 #include "Imu.h"
 #include "Motor.h"
 #include "Calculates.h"
@@ -134,6 +134,125 @@ void loop()
   // Serial.println(pidvel);
   Serial.println(calcRPM);
   Serial.println(ecdRPM);
+}
   */
+#include "MPU9250.h"
+
+MPU9250 mpu;
+
+void print_roll_pitch_yaw() {
+    Serial.print("accx, accy, accz: ");
+    Serial.print(mpu.getAccX(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getAccY(), 2);
+    Serial.print(", ");
+    Serial.println(mpu.getAccZ(), 2);
+}
+void setup() {
+    Serial.begin(115200);
+    Wire.begin();
+    delay(2000);
+
+    if (!mpu.setup(0x68)) {  // change to your own address
+        while (1) {
+            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+            delay(5000);
+        }
+    }
 }
 
+void loop() {
+    if (mpu.update()) {
+        static uint32_t prev_ms = millis();
+        if (millis() > prev_ms + 25) {
+            print_roll_pitch_yaw();
+            prev_ms = millis();
+        }
+    }
+}
+/*
+#include "MPU9250.h"
+
+uint8_t addrs[7] = {0};
+uint8_t device_count = 0;
+
+template <typename WireType = TwoWire>
+void scan_mpu(WireType& wire = Wire) {
+    Serial.println("Searching for i2c devices...");
+    device_count = 0;
+    for (uint8_t i = 0x68; i < 0x70; ++i) {
+        wire.beginTransmission(i);
+        if (wire.endTransmission() == 0) {
+            addrs[device_count++] = i;
+            delay(10);
+        }
+    }
+    Serial.print("Found ");
+    Serial.print(device_count, DEC);
+    Serial.println(" I2C devices");
+
+    Serial.print("I2C addresses are: ");
+    for (uint8_t i = 0; i < device_count; ++i) {
+        Serial.print("0x");
+        Serial.print(addrs[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.println();
+}
+
+template <typename WireType = TwoWire>
+uint8_t readByte(uint8_t address, uint8_t subAddress, WireType& wire = Wire) {
+    uint8_t data = 0;
+    wire.beginTransmission(address);
+    wire.write(subAddress);
+    wire.endTransmission(false);
+    wire.requestFrom(address, (size_t)1);
+    if (wire.available()) data = wire.read();
+    return data;
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.flush();
+    Wire.begin();
+    delay(2000);
+
+    scan_mpu();
+
+    if (device_count == 0) {
+        Serial.println("No device found on I2C bus. Please check your hardware connection");
+        while (1)
+            ;
+    }
+
+    // check WHO_AM_I address of MPU
+    for (uint8_t i = 0; i < device_count; ++i) {
+        Serial.print("I2C address 0x");
+        Serial.print(addrs[i], HEX);
+        byte ca = readByte(addrs[i], WHO_AM_I_MPU9250);
+        if (ca == MPU9250_WHOAMI_DEFAULT_VALUE) {
+            Serial.println(" is MPU9250 and ready to use");
+        } else if (ca == MPU9255_WHOAMI_DEFAULT_VALUE) {
+            Serial.println(" is MPU9255 and ready to use");
+        } else if (ca == MPU6500_WHOAMI_DEFAULT_VALUE) {
+            Serial.println(" is MPU6500 and ready to use");
+        } else {
+            Serial.println(" is not MPU series");
+            Serial.print("WHO_AM_I is ");
+            Serial.println(ca, HEX);
+            Serial.println("Please use correct device");
+        }
+        static constexpr uint8_t AK8963_ADDRESS {0x0C};  //  Address of magnetometer
+        static constexpr uint8_t AK8963_WHOAMI_DEFAULT_VALUE {0x48};
+        byte cb = readByte(AK8963_ADDRESS, AK8963_WHO_AM_I);
+        if (cb == AK8963_WHOAMI_DEFAULT_VALUE) {
+            Serial.print("AK8963 (Magnetometer) is ready to use");
+        } else {
+            Serial.print("AK8963 (Magnetometer) was not found");
+        }
+    }
+}
+
+void loop() {
+}
+*/
