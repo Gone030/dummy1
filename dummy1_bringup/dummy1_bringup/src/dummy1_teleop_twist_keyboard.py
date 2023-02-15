97b1f19ddd29d3b7609f568cf9d212763b575c4a
Gone030 <gjkjw_030@naver.com>
2023-02-15 16:00:30 +0900

    update teleop

--- dummy1_bringup/dummy1_bringup/src/dummy1_teleop_twist_keyboard.py
+++ dummy1_bringup/dummy1_bringup/src/dummy1_teleop_twist_keyboard.py
@@ -1,14 +1,11 @@
 from __future__ import print_function
 
+import sys
+
 import rclpy
 from geometry_msgs.msg import Twist
-from pynput.keyboard import Key, Listener
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
-
-import os
-import select
-import sys
 
 if sys.platform == 'win32':
     import msvcrt
@@ -59,120 +56,100 @@ ctl + c : quit
 e = """
 Connection Failed
 """
+def getKey(settings):
+    if sys.platform =='win32':
+        key = msvcrt.getwch()
+    else:
+        tty.setraw(sys.stdin.fileno())
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
-class teleop(Node):
-    def __init__(self):
-        super().__init__('dummy1_teleop_twist_keyboard')
-        qos_profile = QoSProfile(depth = 10)
+def vels(speed, turn):
+    return 'currently:\tspeed %s\tturn %s ' %(speed, turn)
 
-        self.settings = self.saveTerminalSettings()
+def constrain(input, low, high):
+    if input < low:
+        input = low
+    elif input > high:
+        input = high
+    else:
+        input = input
+    return input
 
-        self.keys = set()
-        self.status = 0
-        self.target_linear_vel = 0.0
-        self.target_angular_vel = 0.0
-        self.pub = self.create_publisher(Twist, 'cmd_vel', qos_profile)
+def saveTerminalSettings():
+    if sys.platform == 'win32':
+        return None
+    return termios.tcgetattr(sys.stdin)
 
-        self.max_linear_vel = 0.2362
-        self.max_angular_vel = 0.5233
+def restoreTerminalSettings(old_settings):
+    if sys.platform == 'win32':
+        return
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
-        self.linear_step_size = 0.02
-        self.angular_step_size = 0.1
+def main():
+    settings = saveTerminalSettings()
 
-        self.speed = 0.05
-        self.turn  = 0.5
+    rclpy.init()
 
+    node = rclpy.create_node('dummy1_teleop_twist')
+    pub = node.create_publisher(Twist, 'cmd_vel', 10)
+
+    speed = 0.05
+    turn = 0.5
+    target_linear_vel = 0.0
+    target_angular_vel = 0.0
+    status = 0.0
+    max_linear_vel = 0.2362
+    max_angular_vel = 0.5233
+
+    try:
         print(msg)
-        print(self.vels(self.speed, self.turn))
-        with Listener(
-            on_press=self.on_press,
-            on_release=self.on_release
-        )as listener:
-            listener.join()
-    def vels(self, speed, turn):
-        return 'currently:\tspeed %s\tturn %s ' %(speed, turn)
-
-    def constrain(self, input, low, high):
-        if input < low:
-            input = low
-        elif input > high:
-            input = high
-        else:
-            input = input
-        return input
-
-    def on_press(self, key):
-        tty.setraw(sys.stdin.fileno())
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-
-        if key == Key.esc:
-            return False
-
-        if hasattr(key, 'char'):
-            key = key.char
-            self.keys.add(key)
-            self.move()
-
-    def on_release(self, key):
-        tty.setraw(sys.stdin.fileno())
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-        if hasattr(key, 'char'):
-            key = key.char
-            if key in self.keys:
-                self.keys.remove(key)
-                self.move()
-    def saveTerminalSettings(self):
-        if sys.platform == 'win32':
-            return None
-        return termios.tcgetattr(sys.stdin)
-
-    def restoreTerminalSettings(self, old_settings):
-        if sys.platform == 'win32':
-            return
-        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-
-    def move(self):
-        try:
-            keys = ''.join(self.keys)
+        print(vels(speed,turn))
+        while True:
+            keys = getKey(settings)
             if keys in moveBindings.keys():
-                self.target_linear_vel = moveBindings[keys][0]
-                self.target_angular_vel = moveBindings[keys][1]
+                target_linear_vel = moveBindings[keys][0]
+                target_angular_vel = moveBindings[keys][1]
             elif keys in stepBindings.keys():
-                self.speed = self.constrain(self.speed + stepBindings[keys][0], - self.max_linear_vel , self.max_linear_vel)
-                self.turn = self.constrain(self.turn + stepBindings[keys][1], -self.max_angular_vel, self.max_angular_vel)
+                speed = constrain(speed + stepBindings[keys][0], - max_linear_vel ,  max_linear_vel)
+                turn = constrain(turn + stepBindings[keys][1], -max_angular_vel, max_angular_vel)
                 if keys == 'k':
-                    self.speed = 0.05
+                    speed = 0.05
                 if keys == 'l':
-                    self.turn = 0.5
-                print(self.vels(self.speed, self.turn))
-                if (self.status == 10):
+                    turn = 0.5
+                print( vels( speed, turn))
+                if (status == 10):
                     print(msg)
-                self.status = (self.status + 1) % 11
+                status = (status + 1) % 11
             else:
-                self.target_linear_vel = 0.0
-                self.target_angular_vel = 0.0
+                target_linear_vel = 0.0
+                target_angular_vel = 0.0
 
             twist = Twist()
-            twist.linear.x = self.target_linear_vel * self.linear_step_size
-            twist.angular.z = self.target_angular_vel * self.angular_step_size
-            self.pub.publish(twist)
-        except Exception as err:
-            print(e)
-            print(err)
+            twist.linear.x =  target_linear_vel *  speed
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z =  target_angular_vel *  turn
+            pub.publish(twist)
+    except Exception as err:
+        print(e)
+        print(err)
 
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-
-def main(args = None):
-    rclpy.init(args=args)
-    node = teleop()
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
+    finally:
         twist = Twist()
         twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
         twist.angular.z = 0.0
-        node.pub.publish(twist)
-        node.destroy_node()
-        rclpy.shutdown()
+        pub.publish(twist)
+
+        restoreTerminalSettings(settings)
+
+
 if __name__=='__main__':
     main()
