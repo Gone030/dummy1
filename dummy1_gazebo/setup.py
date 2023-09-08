72ffdf3dd4fe3d29ece359c414a5dabc5ab6e986
Jiwon Kim <gjkjw_030@kyonggi.ac.kr>
2023-09-08 18:11:20 +0900

    first commit for gazebo

---
+++ dummy1_gazebo/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'dummy1_gazebo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.parh.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.sdf'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'hook'), glob('hook/*.sh')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jiwon Kim',
+    maintainer_email='gjkjw_030@kyonggi.ac.kr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+
+        ],
+    },
+)
